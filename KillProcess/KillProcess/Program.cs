abdc6db2bd9135fafafee95ab07f2992836b1dc0
Igor <ikhodos@surroundtech.com>
2012-05-04 15:32:52 -0400

    Initial Commit

---
+++ KillProcess/KillProcess/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+namespace KillProcess
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var processes = new List<Process>();
+            foreach (var s in args)
+            {
+                processes.AddRange(Process.GetProcessesByName(s));
+            }
+            foreach (var process in processes)
+            {
+                Console.WriteLine("Killing - {0}", process.ProcessName);
+                process.Kill();
+            }
+            Console.WriteLine("Done!");
+            //Console.ReadKey();
+        }
+    }
+}
